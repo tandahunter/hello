bb62a02105d0ff808ddbd44724c60788c844d9c3
tandahunter <adam_jones_1985@hotmail.com>
2017-07-03 07:27:56 +0100

    Chapter 1

--- hello.go
+++ hello.go
@@ -2,10 +2,38 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"strings"
 
 	"github.com/tandahunter/stringutil"
 )
 
 func main() {
-	fmt.Printf(stringutil.Reverse("!oG ,olleH"))
+	sayHello()
+	printFirstArgument()
+	printAllArguments()
+	printAllArgumentsFast()
+}
+
+func sayHello() {
+	fmt.Println(stringutil.Reverse("!oG, olleH"))
+}
+
+func printFirstArgument() {
+	if len(os.Args) > 1 {
+		fmt.Println(os.Args[1])
+	} else {
+		fmt.Println("No args")
+	}
+}
+
+func printAllArguments() {
+	for _, a := range os.Args[1:] {
+		fmt.Printf("%s ", a)
+	}
+	fmt.Println()
+}
+
+func printAllArgumentsFast() {
+	fmt.Printf("%s", strings.Join(os.Args[1:], " "))
 }
